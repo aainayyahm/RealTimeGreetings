b427bd3efdf2b699c027402c327ff25c58c56c39
aainayyahm <129457835+aainayyahm@users.noreply.github.com>
2023-08-13 18:54:42 +0700

    Initial Commit

---
+++ RealTimeGreetings/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RealTimeGreetings
+//
+//  Created by Ayla Ainayyah M on 13/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealTimeGreetings/RealTimeGreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeGreetingsApp.swift
+//  RealTimeGreetings
+//
+//  Created by Ayla Ainayyah M on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeGreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
